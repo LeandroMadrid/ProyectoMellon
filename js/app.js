42e02c4090064617abdf90c72fe4d0e94bab8514
Leandro Madrid <leandro.madrid@gmail.com>
2021-09-04 13:57:56 -0300

    Proyecto Final

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
+
+function toogleMenu() {
+    menu.classList.toggle("menu_opened");
+}
+
+openMenuBtn.addEventListener("click", toogleMenu);
+closeMenuBtn.addEventListener("click", toogleMenu);
+
+
